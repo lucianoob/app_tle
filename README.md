5571f5d9145c7109873a0e2b417cc6e164629901
Luciano O. Borges <luciano.ob@gmail.com>
2019-01-28 15:55:19 -0200

    Create install script. Insert infos in Readme.md.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# app_tle
+# App TLE
+
+Este projeto é um exemplo de uma API de controle de gastos usando Laravel, Boostrap 4 e jQuery.
+
+## Pré-Requisitos
+
+Para rodar este projeto deve-se utilizar os seguintes requisitos:
+- [Composer](https://getcomposer.org/)
+- [PHPUnit](https://phpunit.de/)
+- [Apache2](https://httpd.apache.org/)
+- [MySQL](https://www.mysql.com/)
+
+## Componentes
+
+Os componentes utilizados neste projeto são:
+- laravel 5.7.22
+- php 7.2
+- mysql 5.7
+- bootstrap 4.2.1
+- jquery 3.3.1
+- DataTables v1.10.18
+- jquery.mask 1.14.15
+- fontawesome v5.6.3
+
+## Instalação
+
+Para instalar basta rodar o script install.sh, lembrando que é necessário ter todos os pré-requisitos instalados.
---
+++ install.sh
@@ -0,0 +1,35 @@
+
+#!/bin/bash
+
+echo -e "\n### Install App TLE Project ###"
+
+echo -e "\n# Start services (Apache/MySQL)"
+sudo service apache2 start
+sudo service mysql start
+
+echo -e "\n# Create database..."
+echo -e "\n Enter user in mysql: "
+read umysql
+echo -e "\n Enter password in mysql:"
+read pmysql
+mysql -u $umysql -p$pmysql <<-EOF
+	DROP DATABASE IF EXISTS app_tle;
+    CREATE DATABASE app_tle;
+EOF
+
+echo -e "\n# Generate key..."
+php artisan key:generate
+
+echo -e "\n# Clear database..."
+php artisan migrate:refresh
+
+echo -e "\n# Make migrations..."
+php artisan migrate
+
+echo -e  "\n# Make seeds..."
+php artisan db:seed
+
+echo -e "\n# Execute feature and unit tests..."
+phpunit --debug
+
+echo -e "\n### Install Complete !!!\n"
